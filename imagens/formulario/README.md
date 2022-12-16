416f5ab8aebc2067a544f69974b8e29addbd2344
antoniogonga <cristinasakaunilab02@gmail.com>
2022-12-16 08:44:03 -0300

    adição de formulário no js

---
+++ imagens/formulario/README.md
@@ -0,0 +1,2 @@
+# projetoTotiHTMLCSSJS
+Neste projeto adicionou-se o Js ao meu projeto inicial em Html e Css
---
+++ imagens/formulario/script.js
@@ -0,0 +1,81 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+//formulário
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+   
+
+    if(usernameValue === '') {
+        setError(username, 'Escreva seu nome');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'É requerido um email');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Escreva um email valido');
+    } else {
+        setSuccess(email);
+    }
+
+    
+    
+
+};
